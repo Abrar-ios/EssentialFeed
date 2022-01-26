ebfe613d8ebba9574e245f43a0f0b1f0d43619b9
Abrar <abrar.haq@discorp.com>
2022-01-26 23:03:17 +0500

    Load  command delivers no images on empty cache

--- EssentialFeedTests/Feed Cache/Helper/FeedStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helper/FeedStoreSpy.swift
@@ -54,7 +54,11 @@ import EssentialFeed
          receivedMessages.append(.retrieve)
      }
      
-     func completeRetrieval(with error: Error, at index: Int = 0){
+     func completeRetrieval(with error: Error, at index: Int = 0) {
          retrievalCompletions[index](error)
+     }
+     
+     func completeRetrievalWithEmptyCache(at index: Int = 0) {
+         retrievalCompletions[index](nil)
      }
 }
